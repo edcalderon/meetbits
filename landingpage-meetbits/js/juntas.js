c54ae040b63fe310c7adb20e75ec5c774f1d83d1
jgarcia <jgarcia@ariqmo.com>
2019-11-23 10:01:44 -0500

    cambios generales

--- landingpage-meetbits/js/juntas.js
+++ landingpage-meetbits/js/juntas.js
@@ -1,4 +1,29 @@
+
+var juntas;
+/*
+function getMeeting(){
+    return new Priomise(function(resolve, reject){
+        console.log("Actualizando Juntas(getMeeting)*******************");
+        // Realizar peticion
+        meetBITS.IDj((err, meetings) => {
+            console.log("Recorriendo junta (Meeting): " + meeting);
+
+            if(meeting.length > 0){
+                meeting.forEach(element => {
+                    
+                });
+            }
+            else{
+                console.log("No se obtuvieron Juntas");
+            }
+            onload.res = iterar(meeting);
+        })
+    });
+}*/
+
+
 function actualizar_juntas(){
+    debugger;
     console.log("Actualizando Juntas*******************");
     meetBITS.IDj((err,n)=>{
         console.log("n:"+n);
@@ -25,17 +50,53 @@ function leer_asistentes(junta){
 
 
 function imprimir_fila(junta,asistentes){
-    var juntas = $('#juntas').DataTable();
-    console.log("asistentes: "+asistentes);
+    
+    if(!juntas)
+        juntas = $('#juntas').DataTable();
+
+    console.log("asistentes: " + asistentes);
     juntas.row.add( [
         junta[0],
         junta[1],
         junta[2],
         junta[3],
         asistentes,
-        ] ).draw( false );
+        '<button type="button" class="btn waves-effect waves-light btn-info" onclick="showQR(' + junta[0] + ')"><span class="fas fa-qrcode"></span> QR</button>',
+    ]).draw(false);
+
+    /*
+        juntas')
+        .append(
+            $("<tr></tr>")
+                .append(
+                    $("<td></td>")
+                        .text(junta[0])
+                )
+                .append(
+                    $("<td></td>")
+                        .text(junta[1])
+                )
+                .append(
+                    $("<td></td>")
+                        .text(junta[2])
+                )
+                .append(
+                    $("<td></td>")
+                        .text(junta[3])
+                )
+                .append(
+                    $("<td></td>")
+                        .text(asistentes)
+                )
+                .append(
+                    $("<td></td>")
+                        .html('<button type="button" class="btn waves-effect waves-light btn-danger" onclick="showQR(' + junta[0] + ')">Ver QR</button>')
+                )
+        )
+    */
+
 }
 
-function a(){
-
+function showQR(juntaID){
+    alert(juntaID);
 }
