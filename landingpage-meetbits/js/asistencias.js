57556072f9bb22d4e3653b0d52f9c38f58818d7d
rafasantama <38086721+rafasantama@users.noreply.github.com>
2019-11-23 10:11:01 -0500

    QR

--- landingpage-meetbits/js/asistencias.js
+++ landingpage-meetbits/js/asistencias.js
@@ -1,52 +1,26 @@
 function actualizar_asistencias(IDjunta){
-    console.log("Actualizando votaciones*******************");
-    meetBITS.IDj2totalIDVss(IDjunta,(err,n)=>{
+    console.log("Actualizando asistencias*******************");
+    meetBITS.IDj2total_asistentes(IDjunta,(err,n)=>{
         console.log("n:"+n);
-        onload.res=iterarIDv(IDjunta, n);
+        onload.res=iterar_asistentes(IDjunta, n);
     })
 }
 
-function iterarIDv(IDjunta, n){
-    console.log("Iterando--------------------------")
+function iterar_asistentes(IDjunta, n){
+    console.log("Iterando asistencias--------------------------")
     for (i=0;i<n;i++){
         console.log("i:"+i);
-        meetBITS.IDj2IDvs(IDjunta,i,(err,IDv)=>{
-            onload.IDv=leer_trues(IDjunta,IDv);
+        meetBITS.IDj2asistentes(IDjunta,i,(err,asistente_address)=>{
+            onload.asistente_address=imprimir_asistencia(asistente_address);
         })
     }
 }
 
-function leer_trues(IDjunta, IDv){
-    meetBITS.IDj_IDv2trues(IDjunta,IDv,(err,trues)=>{
-        console.log("trues: "+trues+"**********");
-        onload.trues=leer_falses(IDjunta,IDv,trues)
-    })
-}
-
-function leer_falses(IDjunta,IDv,trues){
-    meetBITS.IDj_IDv2falses(IDjunta,IDv,(err,falses)=>{
-        console.log("falses: "+falses+"**********");
-        onload.falses=leer_temaV(IDjunta,IDv,trues, falses)
-    })
-}
-
-function leer_temaV(IDjunta,IDv,trues, falses){
-    meetBITS.IDv2tema(IDv,(err,temaV)=>{
-        console.log("IDv: "+temaV+"**********");
-        onload.temaV=imprimir_votacion(IDjunta,IDv,trues, falses, temaV)
-    })
-}
-
-function imprimir_votacion(IDjunta,IDv,trues, falses, temaV){
-    var votaciones = $('#votaciones').DataTable();
-    console.log("tema IDv: "+temaV+"**********");
-    console.log("Imprimiendo tabla votacion*************");
-    total=trues+falses;
-    votaciones.row.add( [
-        IDjunta,
-        temaV,
-        trues,
-        falses,
-        total,
+function imprimir_asistencia(asistente_address){
+    var asistentes = $('#asistentes').DataTable();
+    console.log("address: "+asistente_address+"**********");
+    console.log("Imprimiendo tabla asistente*************");
+    asistentes.row.add( [
+        asistente_address,
         ] ).draw( false );
 }
